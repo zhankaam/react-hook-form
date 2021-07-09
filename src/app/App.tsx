34ac5b49da290512a549d8cf265f3c3eee22c9f8
zhankaam <zhankaam@gmail.com>
2021-07-09 11:45:57 +0300

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css'
+import {AppBar, Button, Container, IconButton, Toolbar, Typography} from '@material-ui/core'
+import {Menu} from '@material-ui/icons'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import { Route } from 'react-router-dom'
+import {Login} from "../features/Login/Login";
+
+function App() {
+
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Route exact path={"/"} render={() => <TodolistsList/>}/>
+                <Route path={"/login"} render={() => <Login/>}/>
+            </Container>
+        </div>
+    )
+}
+
+export default App
